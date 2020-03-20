1781dde7cce12259c31ffed49adff1f077aaffe1
moontowhee <moontowhee@gmail.com>
2020-03-20 23:49:28 +0100

    Update text

---
+++ Building playful worlds/visserdanielle_bpw-eindopdracht_v1.current/Assets/Scripts/displayUI.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class displayUI : MonoBehaviour
+{
+    public string myString;
+    public Text myText;
+    public float fadeTime;
+    public bool displayInfo;
+
+    // Initialization
+    void Start()
+    {
+        myText = GameObject.Find ("Text").GetComponent<Text> ();
+        myText.color = Color.clear;
+        //Screen.showCursor = false;
+        //Screen.LockCursor = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FadeText();
+
+        /*if (Input.getKeyDown (KeyCode.Escaoe))
+        {
+            Screen.LockCursor = false;
+        }
+        */
+    }
+
+    void OnMouseOver()
+    {
+        displayInfo = true;
+    }
+
+    void OnMouseExit()
+    {
+        displayInfo = false;
+    }
+
+    void FadeText()
+    {
+        if(displayInfo)
+        {
+            myText.text = myString;
+            myText.color = Color.Lerp (myText.color, Color.white, fadeTime * Time.deltaTime);
+        }
+
+        else
+        {
+            myText.color = Color.Lerp (myText.color, Color.clear, fadeTime * Time.deltaTime);
+        }
+    }
+}
