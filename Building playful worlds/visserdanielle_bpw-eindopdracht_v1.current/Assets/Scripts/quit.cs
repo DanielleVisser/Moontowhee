5c872697293568c9cbd36ca59a896ac3bb95d9fb
moontowhee <moontowhee@gmail.com>
2020-03-27 01:15:01 +0100

    Ending screen fix

---
+++ Building playful worlds/visserdanielle_bpw-eindopdracht_v1.current/Assets/Scripts/quit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quit : MonoBehaviour
+{
+    void OnGUI() //Mouse appears is UI
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void doquit()
+    {
+        Debug.Log("has quit game");
+        Application.Quit();
+    }
+
+}
