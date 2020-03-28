5b644445b6ef4c901139491047bd0d51e42b3e96
moontowhee <moontowhee@gmail.com>
2020-03-28 01:19:39 +0100

    Update loading screen

---
+++ Building playful worlds/visserdanielle_bpw-eindopdracht_v1.current/Assets/Scripts/Tests/SceneLoading.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoading : MonoBehaviour
+{
+    [SerializePrivateVariables]
+    public Image _progressBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //start async operation 
+        StartCoroutine(LoadAsyncOperation());
+    }
+
+    IEnumerator LoadAsyncOperation()
+    {
+        AsyncOperation gameLevel = SceneManager.LoadSceneAsync(2);
+
+        while (gameLevel.progress < 1)
+        {
+            _progressBar.fillAmount = gameLevel.progress;
+            yield return new WaitForEndOfFrame();
+        }
+        
+    }
+}
