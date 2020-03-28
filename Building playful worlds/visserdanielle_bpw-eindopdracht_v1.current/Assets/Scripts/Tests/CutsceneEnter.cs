e208c5fffad7b008ed866ab4c2487b1e7817d098
moontowhee <moontowhee@gmail.com>
2020-03-28 16:12:56 +0100

    Update final

---
+++ Building playful worlds/visserdanielle_bpw-eindopdracht_v1.current/Assets/Scripts/Tests/CutsceneEnter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneEnter : MonoBehaviour
+{
+    public GameObject thePlayer;
+    public GameObject cutsceneCam;
+
+    void OnTriggerEnter(Collider other)
+    {
+        this.gameObject.GetComponent<BoxCollider>().enabled = false;
+        cutsceneCam.SetActive(true); 
+        thePlayer.SetActive(false);
+        StartCoroutine(FinishCut());
+    }
+
+    IEnumerator FinishCut()
+    {
+        yield return new WaitForSeconds(45);
+        thePlayer.SetActive(true);
+        cutsceneCam.SetActive(false);
+    }
+}
